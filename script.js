34cb635f62e08a20865327b2d02aa7472b2a5b64
technology12 <43604938+technology12@users.noreply.github.com>
2018-09-26 19:43:08 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,117 @@
+$(function() {
+
+
+//道具箱の画像クリックでハリネズミ
+    $("#shownezumi").click(function(){
+      $("#nezumi").fadeIn();
+    });
+
+    $("#nezumi").click(function(){
+      $(this).fadeOut();
+    });
+
+
+    $("#shownezumi2").click(function(){
+      $("#nezumi2").fadeIn();
+    });
+
+    $("#nezumi2").click(function(){
+      $(this).fadeOut();
+    });
+
+
+
+    $("#shownezumi3").click(function(){
+      $("#nezumi3").fadeIn();
+    });
+
+    $("#nezumi3").click(function(){
+      $(this).fadeOut();
+    });
+
+
+//道具の説明をだす
+    $("#kiduti").click(function(){
+      $("#kiduti-e").fadeIn();
+    });
+    $("#kiduti-e").click(function(){
+      $(this).fadeOut();
+    });
+
+
+    $("#sujikebiki").click(function(){
+      $("#sujikebiki-e").fadeIn();
+    });
+    $("#sujikebiki-e").click(function(){
+      $(this).fadeOut();
+    });
+
+    $("#kanna").click(function(){
+      $("#kanna-e").fadeIn();
+    });
+    $("#kanna-e").click(function(){
+      $(this).fadeOut();
+    });
+
+    $("#nomi").click(function(){
+      $("#nomi-e").fadeIn();
+    });
+    $("#nomi-e").click(function(){
+      $(this).fadeOut();
+    });
+
+    $("#sukoya").click(function(){
+      $("#sukoya-e").fadeIn();
+    });
+    $("#sukoya-e").click(function(){
+      $(this).fadeOut();
+    });
+
+    $("#aburatubo").click(function(){
+      $("#aburatubo-e").fadeIn();
+    });
+    $("#kiduti-e").click(function(){
+      $(this).fadeOut();
+    });
+
+    $("#ryoubanokogiri").click(function(){
+      $("#ryoubanokogiri-e").fadeIn();
+    });
+    $("#ryoubanokogiri-e").click(function(){
+      $(this).fadeOut();
+    });
+
+    $("#gennou").click(function(){
+      $("#gennou-e").fadeIn();
+    });
+    $("#gennou-e").click(function(){
+      $(this).fadeOut();
+    });
+
+
+//製作クリックでハリネズミ
+    $("#link-right1").click(function(){
+      $("#hari1").fadeIn();
+    });
+
+    $("#hari1").click(function(){
+      $(this).fadeOut();
+    });
+
+    $("#link-right2").click(function(){
+      $("#hari2").fadeIn();
+    });
+
+    $("#hari2").click(function(){
+      $(this).fadeOut();
+    });
+
+    $("#link-right3").click(function(){
+      $("#hari3").fadeIn();
+    });
+
+    $("#hari3").click(function(){
+      $(this).fadeOut();
+    });
+
+});
